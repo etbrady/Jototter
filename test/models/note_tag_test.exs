23d1a10beb50ccc77b60ddd48ed9e1a3dca3941b
Unknown <etbrady@outlook.com>
2018-03-13 23:33:51 -0500

    Remove broken test

--- test/models/note_tag_test.exs
+++ test/models/note_tag_test.exs
@@ -4,15 +4,10 @@ defmodule Jototter.NoteTagTest do
   alias Jototter.NoteTag
 
   @valid_attrs %{}
-  @invalid_attrs %{}
 
   test "changeset with valid attributes" do
     changeset = NoteTag.changeset(%NoteTag{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = NoteTag.changeset(%NoteTag{}, @invalid_attrs)
-    refute changeset.valid?
-  end
 end
