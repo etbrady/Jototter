3dae56f2e38a32b83fa3038b675f3651578381c4
Unknown <etbrady@outlook.com>
2018-03-18 20:33:26 -0500

    Dynamically compose note query

--- web/models/notes.ex
+++ web/models/notes.ex
@@ -2,17 +2,22 @@ defmodule Jototter.Notes do
     import Ecto.Query
 
     alias Jototter.{Note,Repo}
-    
-    def list_notes(%{:tag => tag}) do 
-        from(n in Note,
-            join: t in assoc(n, :tags),
-            where: t.label == ^tag.label
-            )
-        |> Repo.all
+
+    defp tag(query, args)
+    defp tag(query, %{:tag => tag}) do 
+        query 
+        |> join(:inner, [n], t in assoc(n, :tags))
+        |> where([_, t], t.label == ^tag.label)
+    end
+    defp tag(query, _args) do 
+        query
     end
 
-    def list_notes(_args) do 
-        Repo.all(Note)
+    def list_notes(args) do 
+        query = 
+            Note 
+            |> tag(args)
+        Repo.all(query)
     end
 
     def find_note(id) do 
